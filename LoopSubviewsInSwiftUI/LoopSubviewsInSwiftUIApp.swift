aa17caa8e0e0283ac19f43abc66e694391312ad1
Mischa <dimic@365farmnet.com>
2024-03-31 13:26:36 +0200

    Initial Commit

---
+++ LoopSubviewsInSwiftUI/LoopSubviewsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopSubviewsInSwiftUIApp.swift
+//  LoopSubviewsInSwiftUI
+//
+//  Created by Milos Dimic on 31.03.24.
+//
+
+import SwiftUI
+
+@main
+struct LoopSubviewsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
